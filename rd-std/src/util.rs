7d501e4cc1b90c5b8d0e744b16cd7b4c650a21d6
spacemeowx2 <spacemeowx2@gmail.com>
2021-12-09 21:57:42 +0800

    feat: add udp_connector

--- rd-std/src/util.rs
+++ rd-std/src/util.rs
@@ -1,7 +1,9 @@
 pub use connect_tcp::connect_tcp;
 pub use connect_udp::connect_udp;
 pub use tcp_channel::TcpChannel;
+pub use udp_connector::UdpConnector;
 
 mod connect_tcp;
 mod connect_udp;
 mod tcp_channel;
+mod udp_connector;
